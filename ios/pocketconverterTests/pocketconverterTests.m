efa29335f26700905ad8fd874e322adbd9a2b035
Guilherme Toti <guilhermetoti@gmail.com>
2020-03-22 07:43:16 -0300

    rename ios folder

--- ios/pocketconverterTests/pocketconverterTests.m
+++ ios/pocketconverterTests/pocketconverterTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface multiconversorTests : XCTestCase
+@interface pocketconverterTests : XCTestCase
 
 @end
 
-@implementation multiconversorTests
+@implementation pocketconverterTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
